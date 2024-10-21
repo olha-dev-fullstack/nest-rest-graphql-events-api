fe9659e26b241dbebb27777e909c6223723155f8
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2024-10-21 13:34:18 +0300

    add basic graphql logic to the example school module

--- src/config/orm.config.ts
+++ src/config/orm.config.ts
@@ -4,6 +4,9 @@ import { Attendee } from './../events/attendee/attendee.entity';
 import { Profile } from './../auth/profile.entity';
 import { User } from './../user/user.entity';
 import { Event } from './../events/event.entity';
+import { Teacher } from 'src/school/teacher.entity';
+import { Subject } from 'src/school/subject.entity';
+import { Course } from 'src/school/course.entity';
 
 export default registerAs(
   'orm.config',
@@ -14,7 +17,7 @@ export default registerAs(
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    entities: [Event, Attendee, User, Profile],
+    entities: [Event, Attendee, User, Profile, Subject, Teacher, Course],
     synchronize: true,
     dropSchema: Boolean(parseInt(process.env.DB_DROP_SCHEMA)),
   }),
---
+++ src/school/teacher.entity.ts
@@ -0,0 +1,32 @@
+import { Column, Entity, ManyToMany, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Subject } from './subject.entity';
+import { Field, ObjectType } from '@nestjs/graphql';
+import { Gender } from './school.types';
+import { Course } from './course.entity';
+
+@Entity()
+@ObjectType()
+export class Teacher {
+  constructor(partial?: Partial<Teacher>) {
+    Object.assign(this, partial)
+  }
+  @PrimaryGeneratedColumn()
+  @Field({ nullable: true })
+  id: number;
+
+  @Column()
+  @Field()
+  name: string;
+
+  @Column({type: "enum", enum: Gender, default: Gender.Other})
+  @Field()
+  gender: Gender;
+
+  @ManyToMany(() => Subject, (subject) => subject.teachers)
+  @Field(() => [Subject])
+  subjects: Promise<Subject[]>;
+
+  @OneToMany(() => Course, (course) => course.teacher)
+  @Field(() => [Course])
+  courses: Promise<Course[]>;
+}
