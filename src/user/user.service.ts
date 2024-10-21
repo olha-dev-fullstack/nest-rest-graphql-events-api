46cb25c4f5ba8ab59b9cbe2f7861ad2123c6b24b
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2024-10-21 16:00:49 +0300

    move retypedPassword validation into a custom validator

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,19 +2,27 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
+import { AuthService } from 'src/auth/auth.service';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
+    private readonly authService: AuthService,
   ) {}
 
   async getOneByUsernameOrEmail(username: string, email: string) {
     return this.userRepository.findOne({ where: [{ username }, { email }] });
   }
 
-  async saveUser(user: User) {
-    return this.userRepository.save(user);
+  public async create(createUserDto: CreateUserDto): Promise<User> {
+    return this.userRepository.save(
+      new User({
+        ...createUserDto,
+        password: await this.authService.hashPassword(createUserDto.password),
+      }),
+    );
   }
 }
